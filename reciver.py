0a33892a91fb9cffed62256cfeb4d0cc780e4621
CathrineChernogor <chernogor_k@mail.ru>
2021-07-09 17:19:18 +0800

    testing reciver -  done

--- reciver.py
+++ reciver.py
@@ -1,9 +1,10 @@
+import base64
 import socket
 import time
 import requests
 import json
 
-gateway_adress = "127.0.0.1"
+gateway_adress = "192.168.1.1"
 gateway_port = 8002
 
 flask_adress = "127.0.0.1"
@@ -21,11 +22,8 @@ def send_post(device_code, value):
 if __name__ == '__main__':
     while True:
         rec, addr = sock.recvfrom(1024)
-        # rec = b'\x02d\xc5\x00\x00\x00\xa8\xe7}\xe4]\x14{"rxpk":[{"tmst":79926988,"chan":0,"rfch":1,
-        # "freq":868.100000,"stat":1,"modu":"LORA","datr":"SF9BW125","codr":"4/5","lsnr":6.8,"rssi":-55,"size":6,
-        # "data":"LTUyNDY3"}]}'
-        rec = rec[12:]
-        rec = rec.decode()
+
+        rec = rec[12:].decode()
         if "rxpk" in rec:
             data = json.loads(rec)
             data = data['rxpk'][0]
@@ -35,6 +33,7 @@ if __name__ == '__main__':
             datr = data["datr"]
             # where should i get value and device code? cuz device code is generated at android app but datr
             # generated at device
+            msg = base64.b64decode(msg)
             send_post(datr, msg)
-            # print(f"post {msg, datr}")
-            time.sleep(1)
+            print(f"send {datr} {msg}")
+        time.sleep(0.5)
